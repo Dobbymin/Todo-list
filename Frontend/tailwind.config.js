e81486a303bc909e02aecab5a9b81d4392e7bb6a
Dobbymin <gmin0701@knu.ac.kr>
2023-12-29 23:00:17 +0900

    ✨ feat: tailwindcss 초기세팅

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'brown-50': '#9B6D3D',
+            },
+        },
+    },
+    plugins: [],
+};
